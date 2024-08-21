18e68b78a4b1472eb1719b1046ba4464fe52cfb9
Andrew Shiroma <amshiroma@zoho.com>
2024-08-21 08:21:28 -0700

    removed navbar on create and sign in

--- app/signin/[[...signin]]/page.js
+++ app/signin/[[...signin]]/page.js
@@ -15,85 +15,9 @@ export default function SignInPage() {
       alignItems="center"
       bgcolor={linen}
     >
-      <Box
-        width="100vw"
-        height="10vh"
-        border="none"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        p={2}
-        bgcolor={purple_main}
-        sx={{
-          mb: 3,
-          boxShadow: "1px 1px 1px black",
-        }}
-      >
-        <Box
-          width="100vw"
-          display="flex"
-          alignItems="center"
-          justifyContent="left"
-        >
-          <Typography>*Logo*</Typography>
-          <Button
-            sx={{
-              border: "1px solid black",
-              borderRadius: "10px",
-              color: "black",
-              bgcolor: purple_light,
-              mx: 3,
-              "&:hover": {
-                bgcolor: purple_light,
-                transform: "scale(1.1)",
-              },
-            }}
-            href="http://localhost:3000/"
-          >
-            Dashboard
-          </Button>
-        </Box>
-        <Box
-          width="30vw"
-          display="flex"
-          alignItems="center"
-          justifyContent="right"
-        >
-          <Button
-            sx={{
-              border: "1px solid black",
-              borderRadius: "10px",
-              color: "black",
-              bgcolor: purple_light,
-              mx: 1,
-              "&:hover": {
-                bgcolor: purple_light,
-                transform: "scale(1.1)",
-              },
-            }}
-            href="/signin"
-          >
-            Sign In
-          </Button>
-          <Button
-            sx={{
-              border: "1px solid black",
-              borderRadius: "10px",
-              color: "black",
-              bgcolor: purple_light,
-              mx: 1,
-              "&:hover": {
-                bgcolor: purple_light,
-                transform: "scale(1.1)",
-              },
-            }}
-            href="/create-account"
-          >
-            Create Account
-          </Button>
-        </Box>
+      <Box mt={10}>
+        <SignIn />
       </Box>
-      <SignIn />
     </Box>
   );
 }
