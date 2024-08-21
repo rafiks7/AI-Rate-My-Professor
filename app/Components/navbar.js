bdd067acdc8981e7266deb74435e674aff04b34d
Andrew Shiroma <amshiroma@zoho.com>
2024-08-21 08:34:36 -0700

    changed color hover

--- app/Components/navbar.js
+++ app/Components/navbar.js
@@ -11,86 +11,69 @@ const purple_main = "#8D6B94";
 const purple_light = "#B185A7";
 
 export default function NavBar() {
-    return (
-        <AppBar position="static">
-            <Toolbar sx={{ bgcolor: purple_main, boxShadow: "2px 2px 2px black"}}>
-                <Box display="flex" alignItems="center" flexGrow={1}>
-                    <a href="http://localhost:3000/"><Image src={logo} width={50} height={50}></Image></a>
-                </Box>
-                {/* <Button
-                    sx={{
-                    border: "1px solid black",
-                    borderRadius: "10px",
-                    color: "black",
-                    bgcolor: purple_light,
-                    mx: 3,
-                    "&:hover": {
-                        bgcolor: purple_light,
-                        transform: "scale(1.1)",
-                    },
-                    }}
-                    href="http://localhost:3000/"
-                >
-                    Dashboard
-                </Button>
-                 */}
-                <SignedOut>
-                    <Button
-                        sx={{
-                        border: "1px solid black",
-                        borderRadius: "10px",
-                        color: "black",
-                        bgcolor: purple_light,
-                        mx: 1,
-                        transition: "200ms",
-                        "&:hover": {
-                            bgcolor: purple_light,
-                            transform: "scale(1.1)",
-                        },
-                        }}
-                        href="/signin"
-                    >
-                        Sign In
-                    </Button>
-                    <Button
-                        sx={{
-                        border: "1px solid black",
-                        borderRadius: "10px",
-                        color: "black",
-                        bgcolor: purple_light,
-                        mx: 1,
-                        transition: "200ms",
-                        "&:hover": {
-                            bgcolor: purple_light,
-                            transform: "scale(1.1)",
-                        },
-                        }}
-                        href="/create-account"
-                    >
-                        Sign Up
-                    </Button>
-                </SignedOut>
-                <SignedIn>
-                    <Button
-                        sx={{
-                        border: "1px solid black",
-                        borderRadius: "10px",
-                        color: "black",
-                        bgcolor: purple_light,
-                        mx: 3,
-                        transition: "200ms",
-                        "&:hover": {
-                            bgcolor: purple_light,
-                            transform: "scale(1.1)",
-                        },
-                        }}
-                        onClick={() => {setDialog(true)}}
-                    >
-                        Add Professor
-                    </Button>
-                    <UserButton/>
-                </SignedIn>
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+      <AppBar position="static">
+        <Toolbar sx={{ bgcolor: purple_main, boxShadow: "2px 2px 2px black"}}>
+          <Box display="flex" alignItems="center" flexGrow={1}>
+            <a href="http://localhost:3000/"><Image src={logo} width={50} height={50}></Image></a>
+          </Box>
+          <SignedOut>
+            <Button
+              sx={{
+              border: "1px solid black",
+              borderRadius: "10px",
+              color: "black",
+              bgcolor: purple_light,
+              mx: 1,
+              transition: "200ms",
+              "&:hover": {
+                bgcolor: linen,
+                transform: "scale(1.05)",
+              },
+              }}
+              href="/signin"
+            >
+              Sign In
+            </Button>
+            <Button
+              sx={{
+              border: "1px solid black",
+              borderRadius: "10px",
+              color: "black",
+              bgcolor: purple_light,
+              mx: 1,
+              transition: "200ms",
+              "&:hover": {
+                bgcolor: linen,
+                transform: "scale(1.05)",
+              },
+              }}
+              href="/create-account"
+            >
+              Sign Up
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <Button
+              sx={{
+              border: "1px solid black",
+              borderRadius: "10px",
+              color: "black",
+              bgcolor: purple_light,
+              mx: 3,
+              transition: "200ms",
+              "&:hover": {
+                  bgcolor: linen,
+                  transform: "scale(1.05)",
+              },
+              }}
+              onClick={() => {setDialog(true)}}
+            >
+              Add Professor
+            </Button>
+            <UserButton/>
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+  )
 }
