403067ac69dbf69e08d1038c717d3d17ef93cb21
Andrew Shiroma <amshiroma@zoho.com>
2024-08-22 08:58:31 -0700

    added explaning comments

--- app/Components/filtertextfield.js
+++ app/Components/filtertextfield.js
@@ -6,7 +6,6 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 //Colors
 const linen = "#FFF4E9";
 const purple_dark = "#8D6B94";
-const purple_mid = "#B185A7";
 const purple_light = "#baa4be";
 
 const textfieldTheme = createTheme({
@@ -14,12 +13,15 @@ const textfieldTheme = createTheme({
     MuiTextField: {
       styleOverrides: {
         root: {
+          // declare color root vars to use later
           '--TextField-brandBorderColor': purple_dark,
           '--TextField-brandBorderHoverColor': purple_light,
           '--TextField-brandBorderFocusedColor': purple_light,
+          // use color for text color
           '& .MuiInputBase-input': {
             color: linen,
           },
+          // color for border, hover, and focus
           '& .MuiOutlinedInput-root': {
             '& fieldset': {
               borderColor: 'var(--TextField-brandBorderColor)',
@@ -38,10 +40,10 @@ const textfieldTheme = createTheme({
 });
 
 export default function FilterTextField(props) {
-  const {placeholder, sx} = props;
+  const {placeholder, sx} = props; // edit these to get more inputs
   return (
     <ThemeProvider theme={textfieldTheme}>
-      <TextField placeholder={placeholder} />
+      <TextField placeholder={placeholder} sx={sx} />
     </ThemeProvider>
   )
 }
